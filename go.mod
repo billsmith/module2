8d7346ef3c32589348836344f348b5a1cd809386
Bill Smith <bsmith@indeed.com>
2019-05-28 05:56:00 -0500

    module2 depends on module1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/billsmith/module2
+
+require github.com/billsmith/module1 v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "github.com/billsmith/module1"
+    "fmt"
+)
+
+func main() {
+     fmt.Println(module1.Inc(0))
+}
