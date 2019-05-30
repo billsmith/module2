673cf9b0b122aae75ca3e3eab61539df73e18521
Bill Smith <bsmith@indeed.com>
2019-05-30 09:54:00 -0500

    Use module3/v2

--- go.mod
+++ go.mod
@@ -2,5 +2,5 @@ module github.com/billsmith/module2
 
 require (
 	github.com/billsmith/module1/v2 v2.0.0
-	github.com/billsmith/module3 v1.0.0
+	github.com/billsmith/module3/v2 v2.0.0
 )
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
     "fmt"
     "github.com/billsmith/module1/v2"
-    "github.com/billsmith/module3"
+    "github.com/billsmith/module3/v2"
 )
 
 func main() {
      fmt.Println(module1.Dec(0))
-     fmt.Println(module3.Mul(16))
+     fmt.Println(module3.Div(16))
 }
